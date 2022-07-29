d06c2b37bf540b340997e8605ec68c576dad759a
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2022-07-29 10:00:41 +0200

    Add SearchController

---
+++ src/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ModelRepoBrowser.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SearchController : ControllerBase
+{
+    private readonly ILogger<SearchController> logger;
+
+    private readonly RepoBrowserContext context;
+
+    public SearchController(ILogger<SearchController> logger, RepoBrowserContext context)
+    {
+        this.logger = logger;
+        this.context = context;
+    }
+
+    [HttpGet]
+    public string Get(string q)
+    {
+        logger.LogInformation("Search with query <{SearchQuery}>", q);
+
+        var models = context.Models
+            .Where(p => p.SearchVector.Matches(EF.Functions.WebSearchToTsQuery("simple", q)))
+            .Select(m => $"{m.Name}, {m.Version}, {m.File}, {{{string.Join(", ", m.Tags)}}}")
+            .ToList();
+
+        return string.Join("\n", models);
+    }
+}
