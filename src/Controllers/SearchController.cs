52c757ef27da06ac57ec2ba0b64d218efa17b1ac
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2022-08-08 14:49:30 +0200

    Escape Like operator Wildcards in search query

--- src/Controllers/SearchController.cs
+++ src/Controllers/SearchController.cs
@@ -22,16 +22,24 @@ public class SearchController : ControllerBase
     {
         logger.LogInformation("Search with query <{SearchQuery}>", q);
 
-        var searchPattern = $"%{q}%";
+        var searchPattern = $"%{EscapeLikePattern(q)}%";
         var models = context.Models
             .Where(m =>
-                EF.Functions.ILike(m.Name, searchPattern)
-                || EF.Functions.ILike(m.Version, searchPattern)
-                || EF.Functions.ILike(m.File, searchPattern)
+                EF.Functions.ILike(m.Name, searchPattern, @"\")
+                || EF.Functions.ILike(m.Version, searchPattern, @"\")
+                || EF.Functions.ILike(m.File, searchPattern, @"\")
                 || m.Tags.Contains(q))
             .Select(m => $"{m.Name}, {m.Version}, {m.File}, {{{string.Join(", ", m.Tags)}}}")
             .ToList();
 
         return string.Join("\n", models);
+    }
+
+    internal string EscapeLikePattern(string pattern)
+    {
+        return pattern
+            .Replace(@"\", @"\\", StringComparison.OrdinalIgnoreCase)
+            .Replace("_", @"\_", StringComparison.OrdinalIgnoreCase)
+            .Replace("%", @"\%", StringComparison.OrdinalIgnoreCase);
     }
 }
---
+++ tests/SearchControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelRepoBrowser.Controllers;
+using Moq;
+
+namespace ModelRepoBrowser;
+
+[TestClass]
+public class SearchControllerTest
+{
+    private Mock<ILogger<SearchController>> loggerMock;
+    private RepoBrowserContext context;
+    private SearchController controller;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        loggerMock = new Mock<ILogger<SearchController>>();
+        context = new RepoBrowserContext(new DbContextOptionsBuilder<RepoBrowserContext>().Options);
+        controller = new SearchController(loggerMock.Object, context);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        context.Dispose();
+        loggerMock.VerifyAll();
+    }
+
+    [TestMethod]
+    public void EscapeLikePattern()
+    {
+        Assert.AreEqual("HOTRANGE", controller.EscapeLikePattern("HOTRANGE"));
+        Assert.AreEqual(@"H\_OT\%AN\\GE", controller.EscapeLikePattern(@"H_OT%AN\GE"));
+    }
+}
