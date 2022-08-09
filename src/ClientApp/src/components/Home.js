2d6a96308616247a9199746319c5d53482aa169b
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-08-09 17:19:47 +0200

    Setup app translation

--- src/ClientApp/src/components/Home.js
+++ src/ClientApp/src/components/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { useTranslation } from "react-i18next";
 
 export function Home() {
+  const { t } = useTranslation("common");
   return (
     <div>
+      <h1>{t("welcome")}</h1>
       <Button>INTERLIS Model Repo Browser</Button>
     </div>
   );
