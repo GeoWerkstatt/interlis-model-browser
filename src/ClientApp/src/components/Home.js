1edb9032afffb39233a66d8099fca071092360b1
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-08-15 15:15:13 +0200

    Add autocomplete for searchbar

--- src/ClientApp/src/components/Home.js
+++ src/ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, IconButton, TextField, Tooltip, Stack } from "@mui/material";
+import { Autocomplete, Box, Button, IconButton, TextField, Tooltip, Stack } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -9,6 +9,8 @@ import { Results } from "./Results";
 export function Home() {
   const { register, handleSubmit, watch, reset } = useForm();
   const [models, setModels] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [searchOptions, setSearchOptions] = useState([]);
 
   async function search(searchString) {
     const response = await fetch("/search?query=" + searchString);
@@ -19,9 +21,24 @@ export function Home() {
     }
   }
 
+  async function getSearchOptions(searchString) {
+    if (searchString.length < 3) {
+      setSearchOptions([]);
+    } else {
+      const response = await fetch("/search?query=" + searchString);
+      if (response.ok) {
+        const models = await response.json();
+        setSearchOptions([...new Set(models.map((m) => m.name))]);
+      }
+    }
+  }
+
   const onSubmit = (data) => search(data.searchInput);
+
   const clear = () => {
     reset({ searchInput: "" });
+    setInputValue("");
+    setSearchOptions([]);
     setModels(null);
   };
 
@@ -31,22 +48,42 @@ export function Home() {
     <Box name="home">
       <form onSubmit={handleSubmit(onSubmit)} name="search-form">
         <Stack mt={20} direction="row" justifyContent="space-between" alignItems="strech">
-          <TextField
-            margin="normal"
-            fullWidth
-            label={t("search-instructions")}
-            variant="outlined"
-            {...register("searchInput", { required: true })}
-            InputProps={{
-              startAdornment: <SearchIcon color="disabled" />,
-              endAdornment: (
-                <Tooltip title={t("reset-search")}>
-                  <IconButton sx={{ visibility: watch("searchInput") !== "" ? "visible" : "hidden" }} onClick={clear}>
-                    <ClearIcon />
-                  </IconButton>
-                </Tooltip>
-              ),
+          <Autocomplete
+            freeSolo
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+              getSearchOptions(newInputValue);
             }}
+            options={searchOptions}
+            fullWidth
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                margin="normal"
+                fullWidth
+                label={t("search-instructions")}
+                variant="outlined"
+                {...register("searchInput", { required: true })}
+                InputProps={{
+                  ...params.InputProps,
+                  style: {
+                    padding: 10,
+                  },
+                  startAdornment: <SearchIcon color="disabled" />,
+                  endAdornment: (
+                    <Tooltip title={t("reset-search")}>
+                      <IconButton
+                        sx={{ visibility: watch("searchInput") !== "" ? "visible" : "hidden" }}
+                        onClick={clear}
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    </Tooltip>
+                  ),
+                }}
+              />
+            )}
           />
           <Button
             sx={{
