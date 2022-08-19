8f3d365c076462c56c8bf9f9ffc5a0a5d7460c28
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-08-19 09:26:57 +0200

    Add embed dialog

--- src/ClientApp/src/components/Layout.js
+++ src/ClientApp/src/components/Layout.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import CodeIcon from "@mui/icons-material/Code";
+import { AppBar, Box, Container, Toolbar, Tooltip, Typography } from "@mui/material";
+import { EmbedDialog } from "./EmbedDialog";
+import { useTranslation } from "react-i18next";
 
 export function Layout(props) {
   const [version, setVersion] = useState();
+  const [open, setOpen] = useState(false);
+  const { t } = useTranslation("common");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("/version");
@@ -19,6 +28,9 @@ export function Layout(props) {
         <AppBar position="static">
           <Toolbar>
             <Typography sx={{ flexGrow: 1 }}>INTERLIS Model Repo Browser</Typography>
+            <Tooltip title={t("generate-embed-tag")}>
+              <CodeIcon sx={{ marginBottom: 0.5, marginRight: 1 }} onClick={handleClickOpen}></CodeIcon>
+            </Tooltip>
             <Typography variant="caption" gutterBottom>
               Version: {version}
             </Typography>
@@ -26,6 +38,7 @@ export function Layout(props) {
         </AppBar>
       </Box>
       <Container>{props.children}</Container>
+      <EmbedDialog open={open} setOpen={setOpen}></EmbedDialog>
     </div>
   );
 }
