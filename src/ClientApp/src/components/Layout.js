4f6d03901fefef5f3261ea5340a24c1be2956592
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-08-30 15:34:10 +0200

    Add select to manually change language

--- src/ClientApp/src/components/Layout.js
+++ src/ClientApp/src/components/Layout.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import CodeIcon from "@mui/icons-material/Code";
-import { AppBar, Box, Container, Toolbar, Tooltip, Typography } from "@mui/material";
+import { AppBar, Box, Container, FormControl, MenuItem, Select, Toolbar, Tooltip, Typography } from "@mui/material";
 import { EmbedDialog } from "./EmbedDialog";
 import { useTranslation } from "react-i18next";
 
 export function Layout(props) {
   const [version, setVersion] = useState();
   const [open, setOpen] = useState(false);
-  const { t } = useTranslation("common");
+  const { t, i18n } = useTranslation("common");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,14 +31,29 @@ export function Layout(props) {
             <Tooltip title={t("generate-embed-tag")}>
               <CodeIcon sx={{ marginBottom: 0.5, marginRight: 1 }} onClick={handleClickOpen}></CodeIcon>
             </Tooltip>
-            <Typography variant="caption" gutterBottom>
-              Version: {version}
-            </Typography>
+            <FormControl variant="standard">
+              <Select
+                disableUnderline
+                sx={{ ml: 3, color: "white", backgroundColor: "none" }}
+                value={i18n.language}
+                onChange={(e) => i18n.changeLanguage(e.target.value)}
+                inputProps={{
+                  styles: { color: "white" },
+                }}
+              >
+                <MenuItem value={"de"}>DE</MenuItem>
+                <MenuItem value={"fr"}>FR</MenuItem>
+                <MenuItem value={"it"}>IT</MenuItem>
+              </Select>
+            </FormControl>
           </Toolbar>
         </AppBar>
       </Box>
       <Container>{props.children}</Container>
       <EmbedDialog open={open} setOpen={setOpen}></EmbedDialog>
+      <Typography sx={{ position: "fixed", bottom: 1, right: 1, color: "darkgrey" }} variant="caption" gutterBottom>
+        Version: {version}
+      </Typography>
     </div>
   );
 }
