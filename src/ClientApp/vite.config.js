b94b7589abd6e80f48a6597ec771cc2dfebe3bba
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-07-08 13:23:42 +0200

    Configure vite proxy

--- src/ClientApp/vite.config.js
+++ src/ClientApp/vite.config.js
@@ -5,6 +5,16 @@ export default defineConfig({
   base: "",
   plugins: [react()],
   server: {
+    proxy: Object.assign(
+      {},
+      ...["/version", "/search", "/model", "/api"].map((path) => ({
+        [path]: {
+          target: "https://localhost:7116",
+          changeOrigin: true,
+          secure: false,
+        },
+      })),
+    ),
     port: 44416,
   },
 });
