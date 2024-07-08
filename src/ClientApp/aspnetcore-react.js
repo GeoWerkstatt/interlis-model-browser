347f1172291b2dbdcff4ac8f61478abcea198754
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2024-07-08 11:59:22 +0200

    Convert prestart scripts to js modules

--- src/ClientApp/aspnetcore-react.js
+++ src/ClientApp/aspnetcore-react.js
@@ -1,8 +1,8 @@
 // This script configures the .env.development.local file with additional environment variables to configure HTTPS using the ASP.NET Core
 // development certificate in the webpack development proxy.
 
-const fs = require('fs');
-const path = require('path');
+import { existsSync, writeFileSync, readFileSync, appendFileSync } from "fs";
+import { join } from "path";
 
 const baseFolder =
   process.env.APPDATA !== undefined && process.env.APPDATA !== ''
@@ -17,17 +17,17 @@ if (!certificateName) {
   process.exit(-1);
 }
 
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+const certFilePath = join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync('.env.development.local')) {
-  fs.writeFileSync(
+if (!existsSync('.env.development.local')) {
+  writeFileSync(
     '.env.development.local',
 `SSL_CRT_FILE=${certFilePath}
 SSL_KEY_FILE=${keyFilePath}`
   );
 } else {
-  let lines = fs.readFileSync('.env.development.local')
+  let lines = readFileSync('.env.development.local')
     .toString()
     .split('\n');
 
@@ -41,13 +41,13 @@ SSL_KEY_FILE=${keyFilePath}`
     }
   }
   if (!hasCert) {
-    fs.appendFileSync(
+    appendFileSync(
       '.env.development.local',
       `\nSSL_CRT_FILE=${certFilePath}`
     );
   }
   if (!hasCertKey) {
-    fs.appendFileSync(
+    appendFileSync(
       '.env.development.local',
       `\nSSL_KEY_FILE=${keyFilePath}`
     );
